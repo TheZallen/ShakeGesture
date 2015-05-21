5b37202481fde29265de377eca2707b11a177334
TheZallen <zak.allen1009@gmail.com>
2015-05-21 14:23:30 -0500

    shake gesture initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shake Gesture Demo App
+
+Simple app that gives feedback to shaking the device. The three types of feedback are:
+1. Visual - Displays a popup
+2. Vibrate - Subtle vibration
+3. Audio - Notification sound
+
--- ShakeTest/ViewController.m
+++ ShakeTest/ViewController.m
@@ -1,4 +1,4 @@
-//
+//h
 //  ViewController.m
 //  ShakeTest
 //
@@ -7,16 +7,64 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioServices.h>
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize segmentedControl;
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+int which;
+
+-(void) viewDidLoad {
+    // Request to turn on accelerometer and begin receiving accelerometer events
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
+}
+
+- (void)orientationChanged:(NSNotification *)notification {
+    // Respond to changes in device orientation
+}
+
+-(void) viewDidDisappear {
+    // Request to stop receiving accelerometer events and turn off accelerometer
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion == UIEventSubtypeMotionShake) {
+        if(segmentedControl.selectedSegmentIndex == 0){
+            [self showAlert];
+        }else if(segmentedControl.selectedSegmentIndex == 1){
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+        }else if(segmentedControl.selectedSegmentIndex == 2){
+            NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"alert" ofType:@"mp3"];
+            SystemSoundID soundID;
+            AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath: soundPath], &soundID);
+            AudioServicesPlaySystemSound (soundID);
+        }
+    }
+}
+
+
+-(void)showAlert
+{
+    UIAlertView *alertView = [[UIAlertView alloc]
+                              initWithTitle:@"ShakeGesture Demo"
+                              message:@"Shake Detected"
+                              delegate:nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+    [alertView show];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +72,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)segmentedControlAction:(id)sender {
+//    if(segmentedControl.selectedSegmentIndex == 0){
+//
+//    }else if(segmentedControl.selectedSegmentIndex == 1){
+//        self.view.backgroundColor = [UIColor greenColor];
+//    }else if(segmentedControl.selectedSegmentIndex == 2){
+//        self.view.backgroundColor = [UIColor blueColor];
+//    }
+}
 @end
